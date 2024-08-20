091a489b2bbbbe58af685e09cbf1e724d55b5abe
sandeep0009 <500sandeepks@gmail.com>
2024-08-20 17:12:38 +0530

    done some minor changes

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,77 @@
+import { NextAuthOptions } from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import bcrypt from 'bcryptjs';
+import prisma from '@/utils/globalPrisma';
+import { PrismaAdapter } from '@auth/prisma-adapter';
 import NextAuth from 'next-auth/next';
-import { authOptions } from './option';
+// import { User } from '@prisma/client';
+
+// interface UserWithRole extends User {
+//   role: string;
+// }
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma) as any, 
+  providers: [
+    CredentialsProvider({
+      id: 'credentials',
+      name: 'Credentials',
+      credentials: {
+        email: { label: 'Email', type: 'text' },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials) {
+        if (!credentials) {
+          return null;
+        }
+
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+
+        if (!user) {
+          return null;
+        }
+
+        const isValidPassword = await bcrypt.compare(credentials.password, user.password);
+
+        if (!isValidPassword) {
+          return null;
+        }
+
+        return { ...user, role: user.role || 'user' };
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+        token.role = user.role;
+      }
+      return token;
+    },
+    async session({ token, session }) {
+      if (token) {
+        session.user.id = token.id;
+        session.user.role = token.role;
+      }
+      return session;
+    },
+  },
+  session: {
+    strategy: 'jwt',
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  pages: {
+    signIn: '/signin',
+  },
+};
+
 
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
+
